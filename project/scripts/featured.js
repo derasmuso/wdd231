9fbd5c400c33dd82e0249d3e71bdfa0b1c119e99
derasmuso <drasmuson@byupathway.edu>
2026-06-12 16:16:36 -0600

    Home page updated.  CSS updated. Activities JS and json added.

---
+++ project/scripts/featured.js
@@ -0,0 +1,64 @@
+const show = document.querySelector("#featured")
+const file = "data/activities.json"
+
+async function getActivityData(file) {
+    try {
+        const response = await fetch(file);
+
+        if (!response.ok) {
+            throw new Error(`HTTP Error: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+
+        const featuredActivities = data.activities.filter(activity => activity.featured);
+
+        displayItems(featuredActivities);
+    }
+    catch (error) {
+        console.error("Error fetching activity data:", error);
+
+        show.innerHTML = `<p>The activities could not be loaded.</p>`;
+
+    }
+
+
+    function displayItems(activities) {
+        activities.forEach(activity => {
+
+            const card = document.createElement("div")
+
+            const image = document.createElement("img")
+            image.src = `images/${activity.image}`;
+            image.alt = activity.name;
+            image.width = 300;
+            image.height = 200;
+            image.loading = "lazy";
+
+
+            const name = document.createElement("h2")
+            name.innerText = activity.name
+
+            const cost = document.createElement("p")
+            cost.innerText = activity.cost
+
+            const address = document.createElement("address")
+            address.innerText = activity.address
+
+            const description = document.createElement("p")
+            description.innerText = activity.description
+
+
+            card.appendChild(image)
+            card.appendChild(name)
+            card.appendChild(cost)
+            card.appendChild(address)
+            card.appendChild(description)
+
+            show.appendChild(card)
+
+        });
+    }
+}
+getActivityData(file);
--- project/scripts/footer.js
+++ project/scripts/footer.js
@@ -1,5 +1,5 @@
 const year = document.querySelector("#currentyear");
 const today = new Date();
-year.innerHTML = `&copy ${today.getFullYear()} Orem Chamber of Commerce`;
+year.innerHTML = `&copy ${today.getFullYear()} Discover Utah County`;
 
 document.getElementById("lastModified").innerHTML = `Last Modification: ${document.lastModified}`;
